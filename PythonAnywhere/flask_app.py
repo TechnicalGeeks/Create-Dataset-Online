bd930c7188aeec5b1b87250e8b391c17fa906f04
Rohan1904 <51150171+Rohan1904@users.noreply.github.com>
2020-10-11 12:22:46 +0530

    Added Link to End

--- PythonAnywhere/flask_app.py
+++ PythonAnywhere/flask_app.py
@@ -4,12 +4,20 @@ from flask import Flask,redirect,url_for,render_template,request,Response
 
 app=Flask(__name__)
 
-# @app.route('/WebCam',methods=['GET','POST'])
-# def video_feed():
-#     """Video streaming route. Put this in the src attribute of an img tag."""
-#     return Response(gen(),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame'),render_template('Thank.html')
+@app.route('/video_feed',methods=['GET','POST'])
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    if request.method=='GET':
+    
+        return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    print("Post")                
+    return render_template('Thank.html')
 
+
+@app.route('/WebCam')
+def stuff():
+    return render_template('Thank.html')
 
 
 
@@ -19,7 +27,7 @@ def index():
         # Handle POST Request here
         select = request.form.get('comp_select')
         print(select)
-        return Response(gen()),render_template('Thank.html')
+        return render_template('WebCam.html')
     year=[{'name':'SE'}, {'name':'TE'}, {'name':'BE'}]
     div = [{'name':'A'}, {'name':'B'}, {'name':'C'}]
     return render_template('index.html',data=year,div=div)
@@ -29,28 +37,29 @@ def index():
 # @app.route('/',method=['GET','POST'])
 # def index():
 #     if request.method == 'POST':
-#         return render_template('index.html')
+#         return render_template('WebCam.html')
 #     """Video streaming home page."""
-#     return render_template('index1.html')
+#     return render_template('index.html')
 
 
 def gen():
     """Video streaming generator function."""
-    cap1 = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
 
-    # set is used to set size  3 = width 4 = height 10 = set briteness 
+    while(cap.isOpened()):
+          # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+            img = cv2.resize(img, (0,0), fx=0.6, fy=0.6) 
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+             
+        else:
+            break       
 
-    while True:
-        # read() returns bool and img a
-        succ2 , img2 =cap1.read()
-        img4 = cv2.resize(img2,(700,480))
-        # To detect Edges canny image = all black with with white borders.
-        # TO convert to Gray Scale
-        # Functions to show image output
-        cv2.imshow("My WEB_ CAM",img4)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break   
+    cap.release()
+
             
 if __name__ == '__main__':
     app.run(debug=True)
