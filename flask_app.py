6afdbd3ca475a18f3cd47f3d060aa0bc0e35b52b
rushabhporwal29 <rushabhporwal29@gmail.com>
2022-05-03 12:53:32 +0530

    Minor Changes

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,9 @@ import time
 import cv2 
 from flask import Flask,redirect,url_for,render_template,request,Response
 import os
+import upload
+
+
 uni_path =''
 app=Flask(__name__)
 
@@ -57,7 +60,7 @@ def gen():
         print (dirs )
         path =os.path.join(root)
     i=0
-    while(cap.isOpened()):
+    while(cap.isOpened() and i<=10):
           # Capture frame-by-frame
         ret, img = cap.read()
         if ret == True:
@@ -73,13 +76,13 @@ def gen():
                 print(str(i)+" Object found. Saving locally.") 
                 cv2.imwrite(str(path)+'/'+str(i)+".jpg",roi_gray) 
                 i=i+1
-
             time.sleep(0.1)
              
         else:
             break       
 
     cap.release()
+    upload.upload1()
 
             
 if __name__ == '__main__':
--- upload.py
+++ upload.py
@@ -1,38 +1,39 @@
 import pyrebase
 import os
 
-config ={
-    "apiKey": "AIzaSyAC4EU24xjMQKXP-41I1TnRDIT4KTN7CV8",
-    "authDomain": "proxy-detection-1df22.firebaseapp.com",
-    "databaseURL": "https://proxy-detection-1df22.firebaseio.com",
-    "projectId": "proxy-detection-1df22",
-    "storageBucket": "proxy-detection-1df22.appspot.com",
-    "messagingSenderId": "17187188207",
-    "appId": "1:17187188207:web:63e8c1f5b50862b1c59a1a",
-    "vmeasurementId": "G-EPTQX1DS4L"
-}
+def upload1():
+    config ={
+        "apiKey": "AIzaSyAC4EU24xjMQKXP-41I1TnRDIT4KTN7CV8",
+        "authDomain": "proxy-detection-1df22.firebaseapp.com",
+        "databaseURL": "https://proxy-detection-1df22.firebaseio.com",
+        "projectId": "proxy-detection-1df22",
+        "storageBucket": "proxy-detection-1df22.appspot.com",
+        "messagingSenderId": "17187188207",
+        "appId": "1:17187188207:web:63e8c1f5b50862b1c59a1a",
+        "vmeasurementId": "G-EPTQX1DS4L"
+    }
 
-firebase = pyrebase.initialize_app(config)
-storage = firebase.storage()
+    firebase = pyrebase.initialize_app(config)
+    storage = firebase.storage()
 
-path ="Dataset"
-root = ""
+    path ="Dataset"
+    root = ""
 
-for (root,dirs,files) in os.walk(path, topdown=True): 
-     print (root )
-     print (dirs )
-     # for images in os.listdir(root):
-     #      path= os.path.join(root,images)
-     #      print(path)
-     #      path_cloud = path
-     #      # storage.child("Dataset").put(str(path))
-     path =os.path.join(root)
-print(path)
-for images in os.listdir(root):
-          path= os.path.join(root,images)
-          path_cloud = path
-          path_cloud = path_cloud.replace('\\','/')
-          print(path_cloud)
-          storage.child(path_cloud).put(str(path))     
+    for (root,dirs,files) in os.walk(path, topdown=True): 
+        print (root )
+        print (dirs )
+        # for images in os.listdir(root):
+        #      path= os.path.join(root,images)
+        #      print(path)
+        #      path_cloud = path
+        #      # storage.child("Dataset").put(str(path))
+        path =os.path.join(root)
+    print(path)
+    for images in os.listdir(root):
+            path= os.path.join(root,images)
+            path_cloud = path
+            path_cloud = path_cloud.replace('\\','/')
+            print(path_cloud)
+            storage.child(path_cloud).put(str(path))     
 
-print("\n******* Files Upload Successfully  *********\n")    
+    print("\n******* Files Upload Successfully  *********\n")    
